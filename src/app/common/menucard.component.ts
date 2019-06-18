beca7566368f3871bfe8a5a3ea7f0f39ca61d2c4
SHASHANK K HOLLA <shashankkholla.ec17@rvce.edu.in>
2019-06-18 06:47:34 +0530

    Transfer from hub to lab

--- src/app/common/menucard.component.ts
+++ src/app/common/menucard.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menucard',
   templateUrl: './menucard.component.html',
   styleUrls: ['./menucard.component.css']
 })
-export class MenucardComponent implements OnInit {
+export class MenucardComponent{
+  @Input() itemName : string;
+  @Input() itemPrice : string;
+  @Input() image : string;
+  @Input() available : boolean;
+  
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+ 
+  
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule, FormControl, FormGroup , Validators} from '@angular/forms';
 import { MatCardModule} from '@angular/material';
 import { Router } from '@angular/router';
 import { ShowOnDirtyErrorStateMatcher} from '@angular/material';
+import { MenucardComponent } from '../common/menucard.component';
 
 @Component({
   selector: 'login',
@@ -14,8 +15,11 @@ import { ShowOnDirtyErrorStateMatcher} from '@angular/material';
 export class LoginComponent implements OnInit{
   loginForm : FormGroup;
   esMatcher = new ShowOnDirtyErrorStateMatcher(); 
-
-
+  
+  itemName : string = "idli";
+  itemPrice : string = "10";
+  isAvailable : boolean = true;
+  url : string = "https://cdn-images-1.medium.com/max/1000/1*v79_JDAJ3VCu6L2sp0Qy9A.jpeg";
   ngOnInit(){
     this.loginForm = new FormGroup({
       username : new FormControl(),
